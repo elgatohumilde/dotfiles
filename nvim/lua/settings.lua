de2ec662bb91c97473b5cf126ee7284bdccfe03f
Joul-qwerty <159632612+Joul-qwerty@users.noreply.github.com>
2025-02-15 11:44:18 -0500

    neovim config

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,32 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.showcmd = true
+vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.g.have_nerd_font = true
+
+vim.schedule(function()
+	vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.api.nvim_create_autocmd({"VimLeave"},{
+    command = "set guicursor=a:ver20",
+})
