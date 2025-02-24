48e19fe192334e66c56c225bc0b0d9180530045e
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-02-24 08:31:52 -0500

    qutebrowser 'kinda' adblock support

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -22,12 +22,34 @@ c.fonts.default_family = 'Caskaydia Cove Nerd Font Mono'
 c.content.autoplay = False
 c.content.cookies.accept = "all"
 c.content.default_encoding = "utf-8"
-c.content.headers.user_agent = (
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
-    " Chrome/80.0.3987.163 Safari/537.36"
-)
+#c.content.headers.user_agent = (
+#    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
+#    " Chrome/80.0.3987.163 Safari/537.36"
+#)
 c.statusbar.widgets = ["keypress", "progress", "url", "scroll"]
 c.content.javascript.clipboard = "access"
+
+# Adblock / Javascript
+c.content.javascript.enabled = True
+c.content.blocking.enabled = True
+c.content.blocking.method = "adblock"
+c.content.blocking.adblock.lists = [
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
 
 # Set Colors
 palette = {
