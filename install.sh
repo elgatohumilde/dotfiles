a16402f2ff568fa2967da699bb90cb88f7073c3d
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-03-02 22:54:21 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@ cd yay
 makepkg -si
 cd ..
 rm -rf yay
-<<<<<<< HEAD
 yay --needed -Sy waybar river tofi gnu-free-fonts ttf-cascadia-code-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common noto-fonts-emoji noto-fonts-cjk xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk stow vim neovim wl-clipboard unzip 7zip wget foot brightnessctl pavucontrol pamixer playerctl ripgrep fd fzf zathura zathura-pdf-mupdf zathura-djvu python python-pip mpv yt-dlp man-db man-pages hyprlock hyprpaper qt5-wayland qt6-wayland thunar vesktop-bin texlive librewolf-bin tree-sitter-cli nodejs syncthing openssh gitu superfile-bin fastfetch eww-git pipewire lib32-pipewire pipewire-audio pipewire-pulse pipewire-docs wireplumber wireplumber-docs
 # yay --needed -Sy rose-pine-gtk-theme-full tokyonight-gtk-theme-git nwg-look
 # sudo ln -s /usr/bin/foot /usr/bin/xterm
-=======
-yay --needed -Sy waybar river tofi ttf-cascadia-code-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common xdg-desktop-portal xdg-desktop-portal-wlr gnu-free-fonts noto-fonts-emoji noto-fonts-cjk stow neovim wl-clipboard unzip 7zip vim foot hyprlock brightnessctl pavucontrol pamixer playerctl  ripgrep fd fzf zathura zathura-pdf-mupdf zathura-djvu gcc-fortran python-pip mpv yt-dlp man-db man-pages hyprpaper qt5-wayland qt6-wayland thunar vesktop-bin texlive librewolf-bin tree-sitter-cli nodejs syncthing openssh gitu
->>>>>>> cf81462a9f9ea8a4deb644c498fd284db72bf9e8
