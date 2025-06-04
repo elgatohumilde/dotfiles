4020bd9fa5bc4753cf1c2a7282844ef67d2e6d66
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-06-04 00:59:01 -0500

    feat: migrate from kitty to wezterm

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -7,7 +7,7 @@ x="$4"
 y="$5"
 
 if file --mime-type "$file" | grep -qE 'image/'; then
-    kitten icat --stdin no --transfer-mode memory --place "${w}x${h}@${x}x${y}" "$1" </dev/null >/dev/tty
+    kitten icat --stdin no --transfer-mode memory --place "${w}x${h}@${x}x${y}" "${file}" </dev/null >/dev/tty
     exit 1
 fi
 
