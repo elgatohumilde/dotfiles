0556e6c98c2a0688350bf43fbe678c0215c92639
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-08-20 03:36:19 -0500

    feat(jdtls): move jdtls-workspace to .cache/jdtls/workspace

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,9 +1,15 @@
 local home = vim.env.HOME
 
+local function get_jdtls_workspace_dir()
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+    return home .. "/.cache/jdtls/workspace/" .. project_name
+end
+
 local config = {
     cmd = {
         "jdtls",
-        "--jvm-arg=-javaagent:" .. home .. "/.local/share/nvim/mason/share/jdtls/lombok.jar"
+        "--jvm-arg=-javaagent:" .. home .. "/.local/share/nvim/mason/share/jdtls/lombok.jar",
+        "-data", get_jdtls_workspace_dir(),
     },
     root_dir = vim.fs.root(0, {
         ".git",
